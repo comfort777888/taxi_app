590a0310e34429da822fb9431eb2d9690654a71d
Aray Akhmetova <99866973+comfort777888@users.noreply.github.com>
2024-05-17 18:47:02 +0500

    Add files via upload

---
+++ components/Home/SearchSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchSection() {
+  return (
+    <div>
+      <p className="text-[20px]">Get a ride</p>
+    </div>
+  );
+}
+
+export default SearchSection;
